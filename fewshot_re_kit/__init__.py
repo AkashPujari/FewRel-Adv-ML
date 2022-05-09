b2444200b3735e483ee47bc8258072d1c7377a2c
AkashPujari <46300572+AkashPujari@users.noreply.github.com>
2022-05-09 00:56:12 -0400

    Update __init__.py

--- fewshot_re_kit/__init__.py
+++ fewshot_re_kit/__init__.py
@@ -1,4 +1,5 @@
 from fewshot_re_kit import data_loader 
-from fewshot_re_kit import framework 
+from fewshot_re_kit import framework
+from fewshot_re_kit import framework_noneuclidean
 from fewshot_re_kit import sentence_encoder
 
